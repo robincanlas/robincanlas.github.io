c89c9ad60a0135f2dd8bde5fb533018acf6c6551
Kristoffer Robin Canlas <robin.canlas@geekgig.com>
2014-11-04 15:40:31 +0800

    second commit

--- movies/public/scripts/controllers/movie_list.js
+++ movies/public/scripts/controllers/movie_list.js
@@ -1,9 +1,9 @@
-$.when(fetchedMovies).done(function(data){
-	this.collection = data;
-	var moviesView = new Movies({collection: this.collection});
-	App.mainRegion.show(moviesView);
+// $.when(fetchedMovies).done(function(data){
+// 	this.collection = data;
+// 	var moviesView = new Movies({collection: this.collection});
+// 	App.mainRegion.show(moviesView);
 
-	// this.listenTo(moviesView, 'childview:show:video', function(){
-	// 	console.log('trigger');
-	// });
-});
+// 	// this.listenTo(moviesView, 'childview:show:video', function(){
+// 	// 	console.log('trigger');
+// 	// });
+// });
