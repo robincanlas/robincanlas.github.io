3aba48e4c8c47478c6e6f9c765f99c27310b1423
Kristoffer Robin Canlas <rukbin011@gmail.com>
2017-05-20 15:34:52 +0800

    modified   css/main.css modified   index.htm modified   js/main.js added      images/flash-run.gif added      images/flash-run1.gif added      images/flash.jpg added      images/port4.jpg added      index - Copy (2).htm added      index - Copy (3).htm added      js/plugins/createjs-2015.11.26.min.js added      js/plugins/createjs.min.js
     - updated the design

--- robincanlas/js/main.js
+++ robincanlas/js/main.js
@@ -7,11 +7,11 @@
 		var self = this;
 		self.mainFact = mainFact;
 
+
 	})
 
 	.factory('mainFact', function(){
-		var self;
-		return self = {
+		var self = {
 			init: function(){
 				self.nav = [
 					{title: 'HOME',link:''},
@@ -19,13 +19,33 @@
 					{title: 'WORK',link:''},
 					{title: 'CONTACTS',link:''},
 				];
+				self.loadFinish = false;
+
+				self.loadQ = new createjs.LoadQueue(true, false, true);
+
+				self.loadQ.loadManifest([
+					{id:'loadImg', src:'./images/flash-run1.gif'},
+					{id:'bg', src:'./images/main-bg.jpg'},
+				]);
+
+				self.loadQ.on('error', function(){
+					console.log('error');
+				});				
+
+				self.loadQ.on('complete', function(){
+					self.loadFinish = true;
+				});
 			},
-		}
+		};
+		return self;
 	})
 
-	.run(function(mainFact){
+	.run(function(mainFact, $timeout){
 		mainFact.init();
 
+		$timeout(function(){
+		},1000);
+			mainFact.loadFinish = true;
 	});
 
 
