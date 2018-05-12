b0e41430cb1f7438f064c4bf441425672a5f9d95
robincanlas <rukbin011@gmail.com>
2018-05-12 21:19:49 +0800

    remove comments

--- projects/serverside/configs/config-root.js
+++ projects/serverside/configs/config-root.js
@@ -1,31 +1,12 @@
-/**
- * 
- * @Author: John Carlo Baybin
- * @Company: Nuevasys
- * @2016
- * @FilePath: /configs/config-root.js
- * @Version: 1.0
- * @Desc: Config script
- * @Changelogs: 
- * 
- **/
+
 var fs = require('node-fs');
 var mongoose = require('mongoose');
 
-/**
- * IP CONFIG
- **/
 exports.appPort = 5000;
 exports.appPortHttp = 5001;
 exports.appIp = "192.168.250.133";
 
-/**
- * KEY
- **/
 exports.EXPRESS_SID_KEY = 'TrainingKey';
 exports.COOKIE_SECRET = 'TrainingCookie';
 
-/**
- * DATABASE
- **/
 mongoose.connect('mongodb://'+exports.appIp+'/sample');
