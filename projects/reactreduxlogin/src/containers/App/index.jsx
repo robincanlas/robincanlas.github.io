67c34511a7a2c5e2dceb109a9e0e7596b73b50fa
Kristoffer.Canlas <kristoffer.canlas@axesnetwork.com>
2019-05-21 13:00:23 +0800

    moved components files to containers

--- projects/reactreduxlogin/src/containers/App/index.jsx
+++ projects/reactreduxlogin/src/containers/App/index.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 import { Router, Route } from 'react-router-dom'
 import { connect } from 'react-redux'
 
-import { history } from '../helpers'
-import { alertActions } from '../actions'
-import { PrivateRoute } from '../components/PrivateRoute'
-import { HomePage} from '../HomePage/HomePage'
-import { LoginPage } from '../LoginPage/LoginPage'
-import { RegisterPage } from '../RegisterPage/RegisterPage'
+import { history } from '../../helpers'
+import { alertActions } from '../../actions'
+import { PrivateRoute } from '../../components/PrivateRoute'
+import { HomePage} from '../HomePage'
+import { LoginPage } from '../LoginPage'
+import { RegisterPage } from '../RegisterPage'
 
 class App extends React.Component {
     constructor(props) {
--- projects/reactreduxlogin/src/index.js
+++ projects/reactreduxlogin/src/index.js
@@ -3,7 +3,7 @@ import { render } from 'react-dom'
 import { Provider } from 'react-redux'
 
 import { store } from './helpers'
-import { App } from './App/App'
+import { App } from './containers/App'
 
 import { configureFakeBackend } from './helpers'
 
